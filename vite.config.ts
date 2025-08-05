0357a4796671bcd06eef66c407b8ab9adb7041e4
master <time_and_work@mail.ru>
2025-08-05 09:19:35 +0300

    sub

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/VegetableStore/',
   test: {
     environment: 'jsdom',
     globals: true,
